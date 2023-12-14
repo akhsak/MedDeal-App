7e44b07a4d0ecac2b2524ab77c292c5f9664b674
akhsak <akhsamariyam2k2 @gmail.com>
2023-12-14 17:35:24 +0530

    add

--- lib/screens/bottomtabs/Addpage.dart
+++ lib/screens/bottomtabs/Addpage.dart
@@ -86,37 +86,70 @@ class _AddpageState extends State<Addpage> {
                 ),
               ),
               SizedBox(height: 20),
-              DropdownButtonFormField(    
-                decoration: InputDecoration(
-                  hintText: 'cateqory',
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-                items: [
-                  DropdownMenuItem(
-                    value: "Meaddeal",
-                    child:Text('Medicines'),
-                     ),
-                     DropdownMenuItem(
-                    value: "Meaddeal",
-                    child:Text('Equipments'),
-                     ),
-                     DropdownMenuItem(
-                    value: "Meaddeal",
-                    child:Text('Machines'),
-                     ),
-                       DropdownMenuItem(
-                    value: "Meaddeal",
-                    child:Text('Others'),
-                     ),
-                ],
-               onChanged: (String? newValue){
-                setState(() {
-                  selectedValue = newValue!;
-                });
-               }),
+              // DropdownButtonFormField(    
+              //   decoration: InputDecoration(
+              //     hintText: 'cateqory',
+              //     border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              //   ),
+              //   items: [
+              //     DropdownMenuItem(
+              //       value: "Meaddeal",
+              //       child:Text('Medicines'),
+              //        ),
+              //        DropdownMenuItem(
+              //       value: "Meaddeal",
+              //       child:Text('Equipments'),
+              //        ),
+              //        DropdownMenuItem(
+              //       value: "Meaddeal",
+              //       child:Text('Machines'),
+              //        ),
+              //          DropdownMenuItem(
+              //       value: "Meaddeal",
+              //       child:Text('Others'),
+              //        ),
+              //   ],
+              //  onChanged: (String? newValue){
+              //   setState(() {
+              //     selectedValue = newValue!;
+              //   });
+              //  }),
                
-              SizedBox(height: 20),
-              Row(
+                DropdownButtonFormField <String>(
+                          decoration: InputDecoration(
+                            hintText: "Select Item",
+                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
+                          ),
+                          dropdownColor: const Color.fromARGB(255, 208, 203, 203),
+                          borderRadius: BorderRadius.circular(30),
+                          isExpanded: true,
+                          onChanged: (String? newvalue) {
+                            setState(() {
+                              selectedValue = newvalue.toString();
+                            });
+                          },
+                          items: const [
+                            DropdownMenuItem(
+                              value: "Medicines",
+                              child:Text("Medicines",style: TextStyle(
+                                color: Color.fromARGB(255, 19, 19, 19)
+                              ),)),
+                            DropdownMenuItem(
+                              value: "Equipments",
+                              child:Text("Equipments",style: TextStyle(color: Color.fromARGB(255, 7, 6, 6)),)),
+                               DropdownMenuItem(
+                              value: "saniters",
+                              child:Text("saniters",style: TextStyle(
+                                color: Color.fromARGB(255, 8, 8, 8)
+                              ),)),
+                            DropdownMenuItem(
+                              value: "others",
+                              child:Text("others",style: TextStyle(color: Color.fromARGB(255, 12, 12, 12)),)),
+                          ],
+                        ),
+
+               SizedBox(height: 20),
+               Row(
                 children: [
                   Expanded(
                     child: Container(
@@ -140,8 +173,8 @@ class _AddpageState extends State<Addpage> {
                     ),
                   ),
                 ],
-              ),
-              SizedBox(height: 20),
+               ),
+               SizedBox(height: 20),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
