42396d689c9da8b66ab417ca916d4982c8147fca
akhsak <akhsamariyam2k2 @gmail.com>
2024-01-11 14:24:20 +0530

    add

--- lib/widget/textformfield.dart
+++ lib/widget/textformfield.dart
@@ -1,40 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
 
-// class CustomTextForm extends StatelessWidget {
-//   final String labelText;
-//   final TextEditingController controller;
-//   final int? maxLength;
-//   final TextInputFormatter? inputFormatters;
-
-
-//   const CustomTextForm(
-//       {super.key, required this.labelText, required this.controller, this.maxLength, this.inputFormatters});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return TextFormField(
-//       controller: controller,
-//       keyboardType: TextInputType.number,
-//       inputFormatters: 
-//         //FilteringTextInputFormatter.digitsOnly
-//         inputFormatters,
-      
-//       decoration: InputDecoration(
-//         labelText: labelText,
-//         prefixText: '₹',
-//         border: OutlineInputBorder(),
-//       ),
-//       validator: (value) {
-//         if (value == null || value.isEmpty) {
-//           return 'value is empty';
-//         } else {
-//           return null;
-//         }
-//       },
-//     );
-//   }
-// }
 
 
 import 'package:flutter/material.dart';
