b58341ce408bb3e80c78fc65853e8a9ce2178850
akhsak <akhsamariyam2k2@gmail.com>
2024-02-08 11:00:45 +0530

    add

--- lib/service/stock_service.dart
+++ lib/service/stock_service.dart
@@ -32,31 +32,3 @@ Future deleteAllitems() async {
 
 
 
-// // Future edit(int id, ItemsModel value) async {
-// //   final itemsDB = await Hive.openBox<ItemsModel>('items_db');
-// //   itemsDB.putAt(id, value);
-// // }
-
-// //   // Future<void> getAllitems(ItemsModel value) async {
-// //   //   final itemsDB = await Hive.openBox<ItemsModel>('items_db');
-// //   //   await itemsDB.add(value);
-// //   // }
-
-// //   Future<void> deleteAllitems() async {
-// //     final itemsDB = await Hive.openBox<ItemsModel>('items_db');
-// //     await itemsDB.clear();
-// // //  getAllitems();
-// //   }
-
-// //   Future<void> edit(int id, ItemsModel value) async {
-// //     final itemsDB = await Hive.openBox<ItemsModel>('items_db');
-// //     itemsDB.putAt(id, value);
-// //   }
-
-// //   Future<void> deleteitems(int id) async {
-// //     final itemsDB = await Hive.openBox<ItemsModel>('items_db');
-// //     itemsDB.deleteAt(id);
-// // //  getAllitems();
-// //   }
-// }
-
